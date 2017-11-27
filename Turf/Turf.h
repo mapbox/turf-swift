7b8bdd0582afd940a050bb8619f559df72dab48c
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-11-27 21:22:45 +0100

    Clean up project settings

--- Turf/Turf.h
+++ Turf/Turf.h
@@ -1,11 +1,11 @@
 #import <Foundation/Foundation.h>
 
-//! Project version number for Geometry.
-FOUNDATION_EXPORT double GeometryVersionNumber;
+//! Project version number for Turf.
+FOUNDATION_EXPORT double TurfVersionNumber;
 
-//! Project version string for Geometry.
-FOUNDATION_EXPORT const unsigned char GeometryVersionString[];
+//! Project version string for Turf.
+FOUNDATION_EXPORT const unsigned char TurfVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Geometry/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Turf/PublicHeader.h>
 
 
