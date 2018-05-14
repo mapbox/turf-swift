e27331ff05c5924054982bd6a6bb6ce6b2d05543
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2018-05-15 00:03:00 +0200

    Document and clean up

--- Sources/Turf/GeoJSON/Geometry.swift
+++ Sources/Turf/GeoJSON/Geometry.swift
@@ -22,54 +22,65 @@ public struct Geometry: Codable {
     }
 }
 
-// Polyline has been renamed to `LineString`. This alias is for backwards compatibility.
+// `Polyline` has been renamed to `LineString`. This alias is for backwards compatibility.
 public typealias Polyline = LineString
 
+/**
+ A `Point` geometry. The `coordinates` property represents a single position.
+ */
 public struct Point: Codable, Equatable {
     var type: String = GeometryType.Point.rawValue
-    var coordinates: CLLocationCoordinate2D
+    public var coordinates: CLLocationCoordinate2D
 }
 
 /**
- A `LineString` struct represents a shape consisting of two or more coordinates,
- specified as `[CLLocationCoordinate2D]`
+ `LineString` geometry represents a shape consisting of two or more coordinates.
  */
 public struct LineString: Codable, Equatable {
     var type: String = GeometryType.LineString.rawValue
-    var coordinates: [CLLocationCoordinate2D]
+    public var coordinates: [CLLocationCoordinate2D]
 }
 
 /**
- A `Polygon` struct represents a shape constisting of a closed `LineString`.
+ A `Polygon` geometry represents a shape constisting of a closed `LineString`.
  */
 public struct Polygon: Codable, Equatable {
     var type: String = GeometryType.Polygon.rawValue
-    var coordinates: [[CLLocationCoordinate2D]]
+    public var coordinates: [[CLLocationCoordinate2D]]
     
     init(coordinates: [[CLLocationCoordinate2D]]) {
         self.coordinates = coordinates
     }
     
-    var innerRings: [Ring]? {
+    public var innerRings: [Ring]? {
         get { return Array(coordinates.suffix(from: 1)).map { Ring(coordinates: $0) } }
     }
     
-    var outerRing: Ring {
+    public var outerRing: Ring {
         get { return Ring(coordinates: coordinates.first! ) }
     }
 }
 
+/**
+ A `MultiPint` geometry. The coordinates property represents a `[CLLocationCoordinate2D]`.
+ */
 public struct MultiPoint: Codable, Equatable {
     var type: String = GeometryType.MultiPoint.rawValue
-    var coordinates: [CLLocationCoordinate2D]
+    public var coordinates: [CLLocationCoordinate2D]
 }
 
+/**
+ A `MultiLineString` geometry. The coordinates property represent a `[CLLocationCoordinate2D]` of two or more coordinates.
+ */
 public struct MultiLineString: Codable, Equatable {
     var type: String = GeometryType.MultiLineString.rawValue
-    var coordinates: [[CLLocationCoordinate2D]]
+    public var coordinates: [[CLLocationCoordinate2D]]
 }
 
+/**
+ A `MultiLineString` geometry. The coordinates property represents a `[LineString]`.
+ */
 public struct MultiPolygon: Codable, Equatable {
     var type: String = GeometryType.MultiLineString.rawValue
-    var coordinates: [[[CLLocationCoordinate2D]]]
+    public var coordinates: [[[CLLocationCoordinate2D]]]
 }
