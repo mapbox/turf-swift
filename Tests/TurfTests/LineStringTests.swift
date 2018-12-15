acf84a46c7c316904a659f15396c0711a8170dff
Minh Nguyễn <mxn@1ec5.org>
2018-12-15 11:37:36 -0800

    Consolidated line string tests

--- Tests/TurfTests/LineStringTests.swift
+++ Tests/TurfTests/LineStringTests.swift
@@ -48,6 +48,17 @@ class LineStringTests: XCTestCase {
     }
     
     func testDistance() {
+        let point1 = CLLocationCoordinate2D(latitude: 20, longitude: 20)
+        let point2 = CLLocationCoordinate2D(latitude: 40, longitude: 40)
+        let line = [point1, point2]
+        
+        let a = LineString(line).distance()
+        XCTAssertEqual(a, 2_928_304, accuracy: 1)
+        
+        // Adapted from: https://gist.github.com/bsudekum/2604b72ae42b6f88aa55398b2ff0dc22
+        let b = LineString(line).distance(from: CLLocationCoordinate2D(latitude: 30, longitude: 30), to: CLLocationCoordinate2D(latitude: 40, longitude: 40))
+        XCTAssertEqual(b, 1_546_971, accuracy: 1)
+        
         // https://github.com/mapbox/turf-swift/issues/27
         let short = CLLocationCoordinate2D(latitude: 49.120403526377203, longitude: -122.6529443631224)
         let long = CLLocationCoordinate2D(latitude: 49.120405, longitude: -122.652945)
