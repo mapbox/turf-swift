ec80a8a2241d97e99d7186ff196a5e35c5feedd5
Jon Nehring <jwn@stonetip.com>
2018-12-19 09:32:39 -0700

    Ported all midpoint tests from Turfjs
    Per https://github.com/Turfjs/turf/blob/master/packages/turf-midpoint/test.js

--- Tests/TurfTests/PointTests.swift
+++ Tests/TurfTests/PointTests.swift
@@ -25,6 +25,8 @@ class PointTests: XCTestCase {
         XCTAssert(geojson.decoded is PointFeature)
     }
     
+    /// Note: All of the midpoint tests use an accuracy of 1 (equal to 1 meter)
+    
     func testMidPointHorizEquator()
     {
         let point1 = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
@@ -34,10 +36,46 @@ class PointTests: XCTestCase {
         XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
     }
     
-    func testMidPointVertEquator()
+    func testMidPointVertFromEquator()
+    {
+        let point1 = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+        let point2 = CLLocationCoordinate2D(latitude: 10.0, longitude: 0.0)
+        
+        let mid = midpoint(point1: point1, point2: point2)
+        XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
+    }
+    
+    func testMidPointVertToEquator()
     {
         let point1 = CLLocationCoordinate2D(latitude: 10.0, longitude: 0.0)
         let point2 = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+        
+        let mid = midpoint(point1: point1, point2: point2)
+        XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
+    }
+    
+    func testMidPointDiagonalBackOverEquator()
+    {
+        let point1 = CLLocationCoordinate2D(latitude: 10.0, longitude: -1.0)
+        let point2 = CLLocationCoordinate2D(latitude: -1.0, longitude: 1.0)
+        
+        let mid = midpoint(point1: point1, point2: point2)
+        XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
+    }
+    
+    func testMidPointDiagonalForwardOverEquator()
+    {
+        let point1 = CLLocationCoordinate2D(latitude: -1.0, longitude: -5.0)
+        let point2 = CLLocationCoordinate2D(latitude: 10.0, longitude: 5.0)
+        
+        let mid = midpoint(point1: point1, point2: point2)
+        XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
+    }
+    
+    func testMidPointLongDistance()
+    {
+        let point1 = CLLocationCoordinate2D(latitude: 21.94304553343818, longitude: 22.5)
+        let point2 = CLLocationCoordinate2D(latitude: 46.800059446787316, longitude: 92.10937499999999)
         
         let mid = midpoint(point1: point1, point2: point2)
         XCTAssertEqual(point1.distance(to: mid), point2.distance(to: mid), accuracy: 1)
