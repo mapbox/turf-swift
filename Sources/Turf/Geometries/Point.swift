683a658208057de99e6779a62da984991c7945bb
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2020-08-26 09:13:11 -0700

    Add documentation to Point.swift

--- Sources/Turf/Geometries/Point.swift
+++ Sources/Turf/Geometries/Point.swift
@@ -5,6 +5,9 @@ import CoreLocation
 
 
 public struct Point: Equatable {
+    /** Note: The pluralization of `coordinates` is defined
+     in the GeoJSON RFC, so we've kept it for consistency.
+     https://tools.ietf.org/html/rfc7946#section-1.5 */
     public var coordinates: CLLocationCoordinate2D
     
     public init(_ coordinates: CLLocationCoordinate2D) {
