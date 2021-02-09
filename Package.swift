5204e4c0906554f656115f55075aa6147caa7752
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-02-09 09:21:20 -0600

    Configure Package.swift to use binary distribution

--- Package.swift
+++ Package.swift
@@ -1,28 +1,21 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
+
+let checksum = "5256bc0056fb35d19f3d4b5a59d9d22e6a2d57e98664e46de656764a71c7d5e0"
+let url = "https://github.com/mapbox/turf-swift/releases/download/v2.0.0-alpha.2/Turf.xcframework.zip"
 
 let package = Package(
     name: "Turf",
+    platforms: [.iOS(.v10), .macOS(.v10_12)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Turf",
             targets: ["Turf"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Turf",
-            dependencies: []),
-        .testTarget(
-            name: "TurfTests",
-            dependencies: ["Turf"]),
+        .binaryTarget(name: "Turf", url: url, checksum: checksum),
     ]
 )
