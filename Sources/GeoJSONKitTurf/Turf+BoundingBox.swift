93b7925a5c7d728b4905d2c88527f5828cdff9a8
Adrian Schoenig <adrian@schoenig.me>
2021-10-15 15:03:41 +1100

    Add logic to create bounding boxes with logic to allow spanning antimeridian (#4)
    Also fixes contains check for bounding boxes that span antimeridian

--- Sources/GeoJSONKitTurf/Turf+BoundingBox.swift
+++ Sources/GeoJSONKitTurf/Turf+BoundingBox.swift
@@ -5,16 +5,27 @@ import GeoJSONKit
 extension GeoJSON.BoundingBox {
   
   public func contains(_ coordinate: GeoJSON.Position, ignoreBoundary: Bool = true) -> Bool {
-    if ignoreBoundary {
+    switch (spansAntimeridian, ignoreBoundary) {
+    case (false, true):
       return southWesterlyLatitude < coordinate.latitude
           && northEasterlyLatitude > coordinate.latitude
           && southWesterlyLongitude < coordinate.longitude
           && northEasterlyLongitude > coordinate.longitude
-    } else {
+    case (false, false):
       return southWesterlyLatitude <= coordinate.latitude
           && northEasterlyLatitude >= coordinate.latitude
           && southWesterlyLongitude <= coordinate.longitude
           && northEasterlyLongitude >= coordinate.longitude
+    case (true, true):
+      return southWesterlyLatitude < coordinate.latitude
+          && northEasterlyLatitude > coordinate.latitude
+          && (southWesterlyLongitude > coordinate.longitude
+           || northEasterlyLongitude < coordinate.longitude)
+    case (true, false):
+      return southWesterlyLatitude <= coordinate.latitude
+          && northEasterlyLatitude >= coordinate.latitude
+          && (southWesterlyLongitude >= coordinate.longitude
+           || northEasterlyLongitude <= coordinate.longitude)
     }
   }
   
@@ -24,5 +35,108 @@ extension GeoJSON.BoundingBox {
       longitude: (southWesterlyLongitude + northEasterlyLongitude) / 2
     )
   }
+  
+  public var spansAntimeridian: Bool {
+    northEasterlyLongitude < southWesterlyLongitude
+  }
+  
+  public init(positions: [GeoJSON.Position], allowSpanningAntimeridian: Bool) {
+    guard allowSpanningAntimeridian, let first = positions.first else {
+      self.init(positions: positions)
+      return
+    }
+    
+    self = positions.dropFirst()
+      .sorted { $0.longitude < $1.longitude }
+      .reduce(into: GeoJSON.BoundingBox(positions: [first])) { box, next in
+        box.append(next, allowSpanningAntimeridian: allowSpanningAntimeridian)
+      }
+  }
+  
+  public init(geometries: [GeoJSON.GeometryObject], allowSpanningAntimeridian: Bool = true) {
+    self.init(positions: geometries.flatMap(\.positions), allowSpanningAntimeridian: allowSpanningAntimeridian)
+  }
+  
+  mutating func append(_ position: GeoJSON.Position, allowSpanningAntimeridian: Bool = true) {
+    guard !contains(position) else { return }
+    let north = max(northEasterlyLatitude, position.latitude)
+    let south = min(southWesterlyLatitude, position.latitude)
+    
+    func distance(to longitude: GeoJSON.Degrees, wrap: Bool) -> GeoJSON.Degrees {
+      if wrap {
+        return abs((position.longitude - longitude).remainder(dividingBy: 360))
+      } else {
+        return abs(position.longitude - longitude)
+      }
+    }
+    
+    let east, west: GeoJSON.Degrees
+    if northEasterlyLongitude >= southWesterlyLongitude, position.longitude <= northEasterlyLongitude, position.longitude >= southWesterlyLongitude {
+      // non-wrapping; longitude covered
+      east = northEasterlyLongitude
+      west = southWesterlyLongitude
+    } else if spansAntimeridian, (position.longitude >= northEasterlyLongitude || position.longitude <= southWesterlyLongitude) {
+      // wrapping; longitude covered
+      east = northEasterlyLongitude
+      west = southWesterlyLongitude
+    } else if position.longitude > northEasterlyLongitude, distance(to: southWesterlyLongitude, wrap: true) < distance(to: northEasterlyLongitude, wrap: false) {
+      // wrapping: position is east, but we extend across west
+      east = northEasterlyLongitude
+      west = position.longitude
+    } else if position.longitude < southWesterlyLongitude, distance(to: northEasterlyLongitude, wrap: true) < distance(to: southWesterlyLongitude, wrap: false) {
+      // wrapping: position is west, but we extend across east
+      east = position.longitude
+      west = southWesterlyLongitude
+    } else {
+      // non-wrapping: extend to include position
+      east = max(position.longitude, northEasterlyLongitude)
+      west = min(position.longitude, southWesterlyLongitude)
+    }
 
+    let minElev, maxElev: GeoJSON.Degrees?
+    if let oldMin = minimumElevation, let oldMax = maximumElevation, let newElev = position.altitude {
+      minElev = min(oldMin, newElev)
+      maxElev = max(oldMax, newElev)
+    } else {
+      minElev = minimumElevation ?? position.altitude
+      maxElev = maximumElevation ?? position.altitude
+    }
+    
+    self = try! .init(coordinates: [
+      west,
+      south,
+      minElev,
+      east,
+      north,
+      maxElev,
+    ].compactMap { $0 })
+  }
+
+}
+
+extension GeoJSON.GeometryObject {
+  public var geometries: [GeoJSON.Geometry] {
+    switch self {
+    case .single(let geo): return [geo]
+    case .multi(let geos): return geos
+    case .collection(let geoObjects): return geoObjects.flatMap(\.geometries)
+    }
+  }
+  
+  public var positions: [GeoJSON.Position] {
+    geometries.flatMap(\.positions)
+  }
+}
+
+extension GeoJSON.Geometry {
+  public var positions: [GeoJSON.Position] {
+    switch self {
+    case .point(let position): return [position]
+    case .lineString(let line): return line.positions
+    case .polygon(let polygon):
+      // Ignore the interior positions as the purpose of this is getting
+      // bounding boxes or alike
+      return polygon.exterior.positions
+    }
+  }
 }
--- Tests/GeoJSONKitTurfTests/BoundingBoxTests.swift
+++ Tests/GeoJSONKitTurfTests/BoundingBoxTests.swift
@@ -10,7 +10,8 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: 1, longitude: 2),
       GeoJSON.Position(latitude: 2, longitude: 1)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
+    XCTAssertFalse(bbox.spansAntimeridian)
     XCTAssertEqual(bbox.southWesterlyLatitude, 1)
     XCTAssertEqual(bbox.southWesterlyLongitude,1)
     XCTAssertEqual(bbox.northEasterlyLatitude, 2)
@@ -22,7 +23,8 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: -1, longitude: -2),
       GeoJSON.Position(latitude: -2, longitude: -1)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
+    XCTAssertFalse(bbox.spansAntimeridian)
     XCTAssertEqual(bbox.southWesterlyLatitude, -2)
     XCTAssertEqual(bbox.southWesterlyLongitude, -2)
     XCTAssertEqual(bbox.northEasterlyLatitude, -1)
@@ -34,7 +36,8 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: 1, longitude: -2),
       GeoJSON.Position(latitude: 2, longitude: -1)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
+    XCTAssertFalse(bbox.spansAntimeridian)
     XCTAssertEqual(bbox.southWesterlyLatitude, 1)
     XCTAssertEqual(bbox.southWesterlyLongitude,-2)
     XCTAssertEqual(bbox.northEasterlyLatitude, 2)
@@ -46,11 +49,131 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: -1, longitude: 2),
       GeoJSON.Position(latitude: -2, longitude: 1)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
+    XCTAssertFalse(bbox.spansAntimeridian)
     XCTAssertEqual(bbox.southWesterlyLatitude, -2)
     XCTAssertEqual(bbox.southWesterlyLongitude, 1)
     XCTAssertEqual(bbox.northEasterlyLatitude, -1)
     XCTAssertEqual(bbox.northEasterlyLongitude, 2)
+  }
+  
+  /// From GeoJSON Specs
+  ///
+  /// Consider a set of point Features within the Fiji archipelago,
+  /// straddling the antimeridian between 16 degrees S and 20 degrees S.
+  /// The southwest corner of the box containing these Features is at 20
+  /// degrees S and 177 degrees E, and the northwest corner is at 16
+  /// degrees S and 178 degrees W.  The antimeridian-spanning GeoJSON
+  /// bounding box for this FeatureCollection is
+  ///
+  /// `"bbox": [177.0, -20.0, -178.0, -16.0]`
+  ///
+  /// and covers 5 degrees of longitude.
+  func testFijiBoundingBox() {
+    let southWest = GeoJSON.Position(latitude: -20, longitude: 177)
+    let northEast = GeoJSON.Position(latitude: -16, longitude: -178)
+    let box = GeoJSON.BoundingBox(positions: [southWest, northEast], allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  northEast.latitude)
+    XCTAssertEqual(box.northEasterlyLongitude, northEast.longitude)
+    XCTAssertEqual(box.southWesterlyLatitude,  southWest.latitude)
+    XCTAssertEqual(box.southWesterlyLongitude, southWest.longitude)
+  }
+  
+  func testAntimeridianCrossExtendingEast() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [175, 9, 176, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+    
+    box.append(.init(latitude: 8, longitude: -178), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, -178)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, 175)
+  }
+  
+  func testAntimeridianCrossExtendingWest() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [-178, 9, -177, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: 179), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, -177)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, 179)
+  }
+  
+  func testExtendingGiantEastAcrossAntimeridian() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [-170, 9, 178, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: -179), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, -179)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, -170)
+  }
+  
+  func testExtendingGiantWestNotAcrossAntimeridian() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [-170, 9, 178, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: -171), allowSpanningAntimeridian: true)
+    XCTAssertFalse(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, 178)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, -171)
+  }
+  
+  func testExtendingGiantWestAcrossAntimeridian() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [-178, 9, 170, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: 179), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, 170)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, 179)
+  }
+  
+  func testExtendingGiantEastNotAcrossAntimeridian() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [-178, 9, 170, 10])
+    XCTAssertFalse(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: 171), allowSpanningAntimeridian: true)
+    XCTAssertFalse(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, 171)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, -178)
+  }
+  
+  func testAntimeridianExtendSouthOnPositive() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [178, 9, -178, 10])
+    XCTAssertTrue(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: 179), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, -178)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, 178)
+  }
+
+  func testAntimeridianExtendSouthOnNegative() throws {
+    var box = try GeoJSON.BoundingBox(coordinates: [178, 9, -178, 10])
+    XCTAssertTrue(box.spansAntimeridian)
+
+    box.append(.init(latitude: 8, longitude: -179), allowSpanningAntimeridian: true)
+    XCTAssertTrue(box.spansAntimeridian)
+    XCTAssertEqual(box.northEasterlyLatitude,  10)
+    XCTAssertEqual(box.northEasterlyLongitude, -178)
+    XCTAssertEqual(box.southWesterlyLatitude,  8)
+    XCTAssertEqual(box.southWesterlyLongitude, 178)
   }
   
   func testContains() {
@@ -59,7 +182,7 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: 0, longitude: 0),
       GeoJSON.Position(latitude: 2, longitude: 2)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
     
     XCTAssertTrue(bbox.contains(coordinate))
   }
@@ -70,7 +193,7 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: 0, longitude: 0),
       GeoJSON.Position(latitude: 2, longitude: 2)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
     
     XCTAssertFalse(bbox.contains(coordinate))
   }
@@ -81,10 +204,17 @@ class BoundingBoxTests: XCTestCase {
       GeoJSON.Position(latitude: 0, longitude: 0),
       GeoJSON.Position(latitude: 2, longitude: 2)
     ]
-    let bbox = GeoJSON.BoundingBox(positions: coordinates)
+    let bbox = GeoJSON.BoundingBox(positions: coordinates, allowSpanningAntimeridian: true)
     
     XCTAssertFalse(bbox.contains(coordinate, ignoreBoundary: true))
     XCTAssertTrue(bbox.contains(coordinate, ignoreBoundary: false))
     XCTAssertFalse(bbox.contains(coordinate))
   }
+  
+  func testAntimeridianContains() throws {
+    let box = try GeoJSON.BoundingBox(coordinates: [175, 8, -179, 10])
+    XCTAssertTrue(box.contains(.init(latitude: 9, longitude: 179)))
+    XCTAssertTrue(box.contains(.init(latitude: 9, longitude: -179)))
+  }
+
 }
