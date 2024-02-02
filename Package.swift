6595a8e74b4c6580937030f8dcba68422141678a
Ivan Persidsky <ivan.persidskii@mapbox.com>
2024-02-02 16:47:12 +0200

    fix visionos identifier

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Turf",
     platforms: [
-        .macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11), .custom("visionOS", versionString: "1.0")
+        .macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11), .custom("visionos", versionString: "1.0")
     ],
     products: [
         .library(
