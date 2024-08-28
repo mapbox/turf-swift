a4aeee2f4e92ac4452a7c58903151b19b19a5bde
Roman Gardukevich <roman.gardukevich@mapbox.com>
2024-08-28 10:36:58 +0300

    XCFramework build script

---
+++ scripts/xcframework.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+TEMPORARY_DIRECTORY=$(mktemp -d)
+echo "Temporary directory: $TEMPORARY_DIRECTORY"
+
+platforms=("iOS" "iOS Simulator" "macOS" "tvOS" "tvOS Simulator" "watchOS" "watchOS Simulator" "visionOS" "visionOS Simulator")
+
+# build Turf for each platform
+
+commands=()
+for platform in "${platforms[@]}"
+do
+  xcodebuild archive \
+    -scheme "Turf" \
+    -archivePath "$TEMPORARY_DIRECTORY/archives/Turf-$platform.xcarchive" \
+    -destination "generic/platform=$platform" \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+  commands+=("-archive" "$TEMPORARY_DIRECTORY/archives/Turf-$platform.xcarchive")
+  commands+=("-framework" "Turf.framework")
+
+done
+
+xcodebuild -create-xcframework "${commands[@]}" -output "$TEMPORARY_DIRECTORY/Turf.xcframework"
+codesign --timestamp -v --sign "Apple Distribution: Mapbox, Inc." "$TEMPORARY_DIRECTORY/Turf.xcframework"
+
+ZIP_OUTPUT_PATH=$(pwd)/Turf.xcframework.zip
+rm -rf "$ZIP_OUTPUT_PATH"
+
+ditto -c -k --sequesterRsrc --keepParent "$TEMPORARY_DIRECTORY/Turf.xcframework" "$ZIP_OUTPUT_PATH"
