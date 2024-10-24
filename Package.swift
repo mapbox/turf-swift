789a0bfc2b50950ed1963b8f612a8ee21eebcd0a
MapboxCI <no-reply@mapbox.com>
2024-10-24 21:28:17 +0300

    Establish release process for Turf binary distribution

--- Package.swift
+++ Package.swift
@@ -9,25 +9,13 @@ let package = Package(
         .macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11), .custom("visionos", versionString: "1.0")
     ],
     products: [
-        .library(
-            name: "Turf",
-            targets: ["Turf"]
-        ),
+        .library(name: "Turf", targets: ["Turf"]),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "Turf",
-            dependencies: [],
-            exclude: ["Info.plist"]
-        ),
-        .testTarget(
-            name: "TurfTests",
-            dependencies: ["Turf"],
-            exclude: ["Info.plist", "Fixtures/simplify"],
-            resources: [
-                .process("Fixtures"),
-            ],
-            swiftSettings: [.define("SPM_TESTING")]
+            url: "https://github.com/mapbox/turf-swift/releases/download/v3.1.0/Turf.xcframework.zip",
+            checksum: "ccc9517866a15f123377e6554953a369e791a1b2cf6ad13ea11f0123dab38be8"
         ),
     ]
 )
--- scripts/xcframework.sh
+++ scripts/xcframework.sh
@@ -2,8 +2,8 @@
 
 set -eou pipefail
 
-TEMPORARY_DIRECTORY=$(mktemp -d)
-echo "Temporary directory: $TEMPORARY_DIRECTORY"
+BUILD_DIRECTORY=${1:-"."}
+echo "Temporary directory: $BUILD_DIRECTORY"
 
 platforms=("iOS" "iOS Simulator" "macOS" "macOS,variant=Mac Catalyst" "tvOS" "tvOS Simulator" "watchOS" "watchOS Simulator" "visionOS" "visionOS Simulator")
 
@@ -14,21 +14,25 @@ for platform in "${platforms[@]}"
 do
   xcodebuild archive \
     -scheme "Turf" \
-    -archivePath "$TEMPORARY_DIRECTORY/archives/Turf-$platform.xcarchive" \
+    -configuration Release \
+    -archivePath "$BUILD_DIRECTORY/archives/Turf-$platform.xcarchive" \
     -destination "generic/platform=$platform" \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
     SUPPORTS_MACCATALYST=YES
 
-  commands+=("-archive" "$TEMPORARY_DIRECTORY/archives/Turf-$platform.xcarchive")
+  commands+=("-archive" "$BUILD_DIRECTORY/archives/Turf-$platform.xcarchive")
   commands+=("-framework" "Turf.framework")
 
 done
 
-xcodebuild -create-xcframework "${commands[@]}" -output "$TEMPORARY_DIRECTORY/Turf.xcframework"
-codesign --timestamp -v --sign "Apple Distribution: Mapbox, Inc." "$TEMPORARY_DIRECTORY/Turf.xcframework"
+xcodebuild -create-xcframework "${commands[@]}" -output "$BUILD_DIRECTORY/Turf.xcframework"
+codesign --timestamp -v --sign "Apple Distribution: Mapbox, Inc." "$BUILD_DIRECTORY/Turf.xcframework"
 
-ZIP_OUTPUT_PATH=$(pwd)/Turf.xcframework.zip
+ZIP_OUTPUT_PATH="$BUILD_DIRECTORY/Turf.xcframework.zip"
 rm -rf "$ZIP_OUTPUT_PATH"
+ditto -c -k --sequesterRsrc --keepParent "$BUILD_DIRECTORY/Turf.xcframework" "$ZIP_OUTPUT_PATH"
 
-ditto -c -k --sequesterRsrc --keepParent "$TEMPORARY_DIRECTORY/Turf.xcframework" "$ZIP_OUTPUT_PATH"
+CHECKSUM=$(swift package compute-checksum "$ZIP_OUTPUT_PATH")
+echo "$CHECKSUM" > "$BUILD_DIRECTORY/xcframework_checksum.txt"
+echo "Checksum: $CHECKSUM"
