fcafcf7fba2d314b5f1ba7eb8e501c813eb82814
MapboxCI <no-reply@mapbox.com>
2024-10-24 21:28:17 +0300

    //wip [skip ci]

--- Package.swift
+++ Package.swift
@@ -9,25 +9,13 @@ let package = Package(
         .macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11), .custom("visionos", versionString: "1.0")
     ],
     products: [
-        .library(
-            name: "Turf",
-            targets: ["Turf"]
-        ),
+        .library(name: "Turf", targets: ["Turf"]),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "Turf",
-            dependencies: [],
-            exclude: ["Info.plist"]
-        ),
-        .testTarget(
-            name: "TurfTests",
-            dependencies: ["Turf"],
-            exclude: ["Info.plist", "Fixtures/simplify"],
-            resources: [
-                .process("Fixtures"),
-            ],
-            swiftSettings: [.define("SPM_TESTING")]
+            url: "https://github.com/mapbox/turf-swift/releases/download/v0.0.0/Turf.xcframework.zip",
+            checksum: "848a007582326855476bdd39d80f78e2d142c6de052fc1e513a13a095647b627" // checksum will be calculated and available in the release baranch from which actual SwiftPM release is done
         ),
     ]
 )
