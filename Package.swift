83847b3a015a792c31473f58b2b202ed99061e21
MapboxCI <no-reply@mapbox.com>
2024-10-25 13:12:25 +0000

    Update versions for Turf.xcframework

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Turf",
-            url: "https://github.com/mapbox/turf-swift/releases/download/v123/Turf.xcframework.zip",
-            checksum: "<checksum>" // checksum will be calculated and available in the release baranch from which actual SwiftPM release is done
+            url: "https://github.com/mapbox/turf-swift/releases/download/v3.2.14/Turf.xcframework.zip",
+            checksum: "a26ce1347b882e56d9b0b85d05f64caa75aac0996b1beaadb57e4acf3fd94fa1" // checksum will be calculated and available in the release baranch from which actual SwiftPM release is done
         ),
     ]
 )
--- Turf.podspec
+++ Turf.podspec
@@ -3,7 +3,7 @@ Pod::Spec.new do |s|
   # ―――  Spec Metadata  ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
 
   s.name = "Turf"
-  s.version = "3.1.0"
+  s.version = "3.2.14"
   s.summary = "Simple spatial analysis."
   s.description = "A spatial analysis library written in Swift for native iOS, macOS, tvOS, watchOS, visionOS, and Linux applications, ported from Turf.js."
 
