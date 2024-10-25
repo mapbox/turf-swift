ed36c08e5910861e492eaf71ddc329216b5dd634
MapboxCI <no-reply@mapbox.com>
2024-10-25 11:45:43 +0000

    Update versions for Turf.xcframework

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Turf",
-            url: "https://github.com/mapbox/turf-swift/releases/download/v123/Turf.xcframework.zip",
-            checksum: "<checksum>" // checksum will be calculated and available in the release baranch from which actual SwiftPM release is done
+            url: "https://github.com/mapbox/turf-swift/releases/download/v3.2.11/Turf.xcframework.zip",
+            checksum: "a0859b5da65b2791ce82de061a778ece45aedf02ef10e41c2d7c2fdc9d7aa387" // checksum will be calculated and available in the release baranch from which actual SwiftPM release is done
         ),
     ]
 )
