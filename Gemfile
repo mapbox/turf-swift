6bb8ee366d81f7e53af39add2b1cbd5238c7ac0c
Aleksei Sapitskii <45671572+aleksproger@users.noreply.github.com>
2024-10-31 11:29:56 +0200

    Establish release process for Turf binary distribution (#243)
    * Establish release process for Turf binary distribution
    
    * Rollback support for Linux

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Fastlane 2.220.0 introduced a new crypto algo for Match, which is not compatible with the pre-existed versions
+gem "fastlane", '= 2.219.0'
--- Package.swift
+++ Package.swift
@@ -3,31 +3,42 @@
 
 import PackageDescription
 
+/// In order to keep Linux comatibility we leave the source based target for Linux.
+/// Apple platforms will use the binary target in order to be compatible with binary dependency in MapboxSDK stack.
+#if canImport(Darwin)
+let targets: [Target] = [
+    .binaryTarget(
+        name: "Turf",
+        url: "https://github.com/mapbox/turf-swift/releases/download/v3.1.0/Turf.xcframework.zip",
+        checksum: "ccc9517866a15f123377e6554953a369e791a1b2cf6ad13ea11f0123dab38be8"
+    )
+]
+#else
+let targets: [Target] = [
+    .target(
+        name: "Turf",
+        dependencies: [],
+        exclude: ["Info.plist"]
+    ),
+    .testTarget(
+        name: "TurfTests",
+        dependencies: ["Turf"],
+        exclude: ["Info.plist", "Fixtures/simplify"],
+        resources: [
+            .process("Fixtures"),
+        ],
+        swiftSettings: [.define("SPM_TESTING")]
+    )
+]
+#endif
+
 let package = Package(
     name: "Turf",
     platforms: [
         .macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11), .custom("visionos", versionString: "1.0")
     ],
     products: [
-        .library(
-            name: "Turf",
-            targets: ["Turf"]
-        ),
+        .library(name: "Turf", targets: ["Turf"]),
     ],
-    targets: [
-        .target(
-            name: "Turf",
-            dependencies: [],
-            exclude: ["Info.plist"]
-        ),
-        .testTarget(
-            name: "TurfTests",
-            dependencies: ["Turf"],
-            exclude: ["Info.plist", "Fixtures/simplify"],
-            resources: [
-                .process("Fixtures"),
-            ],
-            swiftSettings: [.define("SPM_TESTING")]
-        ),
-    ]
+    targets: targets
 )
